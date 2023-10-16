1733c8a54e4e7ffdb305bbdabc1cde6534c21d6f
Pix-Hell <119054517+Pix-Hell@users.noreply.github.com>
2023-10-16 10:24:44 +0200

    initial commit

---
+++ Assets/Scripts/Runtime/PlaceOnSpline.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceOnSpline : MonoBehaviour
+{
+
+    public Spline _spline;
+
+    [SerializeField,Range(0,100)] private float _distanceBetweenObject = 1;
+
+    [SerializeField] private GameObject _repeteadObject;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_spline == null)
+            return;
+
+        if (_repeteadObject == null)
+            return;
+
+        for (float distance = 0; distance < _spline.length(); distance += _distanceBetweenObject)
+        {
+            Vector3 position = _spline.transform.TransformPoint(_spline.computePointWithLength(distance));
+            Orientation orientation = _spline.computeOrientationWithLength(distance, Vector3.up);
+
+            Quaternion rotation = Quaternion.LookRotation(_spline.transform.TransformDirection(orientation.forward), _spline.transform.TransformDirection(orientation.upward));
+            GameObject.Instantiate(_repeteadObject, position, rotation, this.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
